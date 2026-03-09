7113d89b398fd94ac05b522fa94a2bd7baea3f7f
Noor <nooressamjx2005@gmail.com>
2026-03-09 19:28:37 +0200

    Mern stack added + tailwing + folder structure + connection to mongodb

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
