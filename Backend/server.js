44cfce6278c4099306eafaa60b249783a416ffde
SheldorOfAzeroth <pehab0666@gmail.com>
2026-03-17 15:29:54 -0700

    tweak
    refactor(server.js): missing .js in import

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
