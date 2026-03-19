3a96f836a98a728e1ecbd342653eec17ff2596a8
Noor <nooressamjx2005@gmail.com>
2026-03-19 19:53:50 +0200

    Refactor: edited the sidebar nav

--- Frontend/my-app/src/Modules/shared/component/SideBarBox.jsx
+++ Frontend/my-app/src/Modules/shared/component/SideBarBox.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const SideBarBox = ({ title, image }) => {
   return (
-    <div className="w-full flex gap-3 py-4 cursor-pointer px-3 items-center  rounded-lg hover:bg-MainBlue/10">
+    <div className="w-full flex gap-3 py-3 cursor-pointer px-3 items-center  rounded-lg hover:bg-MainBlue/10">
       <div className={`w-4.5 `}>
+        
         <img className="h-full w-full object-cover" src={image} />
       </div>
       <h4 className="text-sm text-MainOffWhiteText font-jakarta font-semibold">
